aa47554fc195c0f0e7e46c99d7c92da8da9603fd
sy <2637065613@qq.com>
2022-12-29 17:57:19 +0800

    2022.12.29

--- service-core/src/main/java/com/atguigu/srb/core/controller/admin/AdminIntegralGradeController.java
+++ service-core/src/main/java/com/atguigu/srb/core/controller/admin/AdminIntegralGradeController.java
@@ -1,12 +1,15 @@
 package com.atguigu.srb.core.controller.admin;
 
 
+import com.atguigu.common.exception.BusinessException;
 import com.atguigu.common.result.R;
+import com.atguigu.common.result.ResponseEnum;
 import com.atguigu.srb.core.pojo.entity.IntegralGrade;
 import com.atguigu.srb.core.service.IntegralGradeService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -24,6 +27,7 @@ import java.util.List;
 @CrossOrigin
 @RestController
 @RequestMapping("/admin/core/integralGrade")
+@Slf4j
 public class AdminIntegralGradeController {
 
     @Resource
@@ -31,25 +35,39 @@ public class AdminIntegralGradeController {
 
     @ApiOperation("积分等级列表")
     @GetMapping("/list")
-    public List<IntegralGrade> listAll(){
-    return  integralGradeService.list();
+    public R listAll(){
+        List<IntegralGrade> list = integralGradeService.list();
+        return R.ok().data("list", list);
     }
 
-    @ApiOperation(value = "根据id删除数据记录",notes = "逻辑删除数据记录")
+    @ApiOperation(value = "根据id删除数据记录", notes = "逻辑删除数据记录")
     @DeleteMapping("/remove/{id}")
-    public boolean removeById(@PathVariable Long id){
-        return integralGradeService.removeById(id);
+    public R removeById(
+            @ApiParam(value = "数据id", required = true, example = "1")
+            @PathVariable Long id){
+        boolean result = integralGradeService.removeById(id);
+        if(result){
+            //return R.setResult(ResponseEnum.UPLOAD_ERROR);
+            return R.ok().message("删除成功");
+        }else{
+            return R.error().message("删除失败");
+        }
     }
 
     @ApiOperation("新增积分等级")
     @PostMapping("/save")
     public R save(
-            @ApiParam(value = "积分等级对象",required = true)
-            @RequestBody IntegralGrade integralGrade){
+            @ApiParam(value = "积分等级对象", required = true)
+            @RequestBody IntegralGrade integralGrade) {
+        if (integralGrade.getBorrowAmount() == null) {
+            throw new BusinessException(ResponseEnum.BORROW_AMOUNT_NULL_ERROR);
+        }
+
+
         boolean result = integralGradeService.save(integralGrade);
-        if(result){
+        if (result) {
             return R.ok().message("保存成功");
-        }else{
+        } else {
             return R.error().message("保存失败");
         }
     }
@@ -57,12 +75,12 @@ public class AdminIntegralGradeController {
     @ApiOperation("根据id获取积分等级")
     @GetMapping("/get/{id}")
     public R getById(
-            @ApiParam(value = "数据id",required = true)
-            @PathVariable String id){
+            @ApiParam(value = "数据id", required = true)
+            @PathVariable String id) {
         IntegralGrade integralGrade = integralGradeService.getById(id);
-        if (integralGrade!=null){
-            return R.ok().data("record",integralGrade);
-        }else{
+        if (integralGrade != null) {
+            return R.ok().data("record", integralGrade);
+        } else {
             return R.error().message("数据获取失败");
         }
     }
@@ -71,19 +89,15 @@ public class AdminIntegralGradeController {
     @ApiOperation("修改积分等级")
     @PutMapping("/update")
     public R updateById(
-            @ApiParam(value = "积分等级对象",required = true)
-            @RequestBody IntegralGrade integralGrade){
+            @ApiParam(value = "积分等级对象", required = true)
+            @RequestBody IntegralGrade integralGrade) {
         boolean result = integralGradeService.updateById(integralGrade);
-        if(result){
+        if (result) {
             return R.ok().message("更新成功");
-        }else{
+        } else {
             return R.error().message("更新失败");
         }
     }
-
-
-
-
 
 
 }
