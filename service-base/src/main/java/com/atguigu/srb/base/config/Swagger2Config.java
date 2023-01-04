0aaf51e34629f66ad920a1388c1255f09bc4b270
sy <2637065613@qq.com>
2023-01-04 17:51:40 +0800

    2023/1/4

--- service-base/src/main/java/com/atguigu/srb/base/config/Swagger2Config.java
+++ service-base/src/main/java/com/atguigu/srb/base/config/Swagger2Config.java
@@ -1,7 +1,12 @@
 package com.atguigu.srb.base.config;
 
+import com.google.common.base.Predicates;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -11,7 +16,24 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class Swagger2Config {
 
     @Bean
-    public Docket apiConfig(){
-        return new Docket(DocumentationType.SWAGGER_2);
+    public Docket apiConfig() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("api")
+                .apiInfo(apiInfo())
+                .select()
+                //只显示admin路径下的页面
+                .paths(Predicates.and(PathSelectors.regex("/api/.*")))
+                .build();
+    }
+        private ApiInfo apiInfo(){
+
+            return new ApiInfoBuilder()
+                    .title("尚融宝-API文档")
+                    .description("本文档描述了尚融宝接口")
+                    .version("1.0")
+                    .contact(new Contact("Helen", "http://atguigu.com", "55317332@qq.com"))
+                    .build();
+
+
     }
 }
